5da3f57c7ef375adc541cd45d6968bdd899027c3
Grace Student <gli_8@berkeley.edu>
2017-06-06 18:50:48 -0400

    first commit

---
+++ Assets/Scripts/DropManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DropManager : MonoBehaviour {
+
+	// Use this for initialization
+    public GameObject player;
+    public GameObject drop;
+    private bool isDropping;
+    private float time;
+
+    void Start () {
+        time = 0;
+	}
+	
+    // Update is called once per frame
+    void Update () {
+        isDropping = Input.GetKey("space");
+        if (isDropping) {
+            time -= Time.deltaTime;
+            if (time <= 0) {
+                Spawn();
+                time = (float) 0.1;
+            } 
+        } else {
+            time = 0;
+        }
+	}
+
+    void Spawn() {
+        Vector3 spawnPoint = player.transform.position;
+        Instantiate(drop, spawnPoint, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PlayerController : MonoBehaviour {
+
+    // Update is before rendering
+    // FixedUpdate is right before any physics calculations
+    
+    public float speed;
+    private Rigidbody2D rigidBody;
+    private bool isSpedUp;
+    private int countFood;
+    private float baseColliderRadius;
+    private CircleCollider2D collider;
+
+    void Start() {
+        rigidBody = GetComponent<Rigidbody2D>();
+        collider = gameObject.GetComponent<CircleCollider2D>();
+        baseColliderRadius = collider.radius;
+        countFood = 0;
+    }
+
+    void Update() {
+        isSpedUp = Input.GetKey("space");
+    }
+    
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVeritcal = Input.GetAxisRaw("Vertical");
+        
+        Vector2 movement = new Vector2(moveHorizontal, moveVeritcal);
+        
+        if (isSpedUp) {
+            rigidBody.AddForce(movement * ((float)(speed * 1.75)));
+        } else {
+            rigidBody.AddForce(movement * speed);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("New Food")) {
+            Destroy(other.gameObject);
+            countFood++;
+            Debug.Log(countFood);
+            float scale = (float) (1 + 0.1 * countFood); 
+            transform.localScale = new Vector3(scale, scale, scale);
+            collider.radius = (float) (baseColliderRadius * (1 + 0.001 * countFood)); 
+        }
+    }
+
+}
